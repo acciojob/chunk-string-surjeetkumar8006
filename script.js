d2cf2ca5033ce472b6867f07aa4380d733d695cd
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2025-09-22 23:17:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
-  // your code here
+  if (str === null) return [];
+  size = Number(size);
+  if (size <= 0) return [];
+
+  const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+  return result;
 }
 
 // Do not change the code below
